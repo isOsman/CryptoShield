bba5f3f039e2a69c30cf02c22d4b72e1284b8078
Osman <niyazovo@inbox.ru>
2020-11-10 18:46:17 +0600

    Added hashing func to CryptoUtils class

--- app/src/main/java/com/osmosoft/cryptoshield/auth/AuthActivity.kt
+++ app/src/main/java/com/osmosoft/cryptoshield/auth/AuthActivity.kt
@@ -2,11 +2,15 @@ package com.osmosoft.cryptoshield.auth
 
 import android.os.Bundle
 import android.text.Editable
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.osmosoft.cryptoshield.R
 import com.osmosoft.cryptoshield.common.AbstractTextWatcher
+import com.osmosoft.cryptoshield.crypto.utils.CryptoUtils
+
+private const val TAG = "AuthActivityTag"
 
 class AuthActivity : AppCompatActivity() {
 
@@ -17,6 +21,8 @@ class AuthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
 
+        Log.d(TAG,CryptoUtils().toMD5Hash("hello"))
+
         passwordEditText = findViewById(R.id.activity_auth_password_edit_text)
         continueButton = findViewById(R.id.activity_auth_continue_button)
 
@@ -24,6 +30,7 @@ class AuthActivity : AppCompatActivity() {
         passwordEditText.addTextChangedListener(object: AbstractTextWatcher(){
             override fun afterTextChanged(s: Editable?) {
                     continueButton.isEnabled = s.toString().isNotEmpty() and s.toString().isNotBlank()
+
             }
         })
 
---
+++ app/src/main/java/com/osmosoft/cryptoshield/crypto/utils/CryptoUtils.kt
@@ -0,0 +1,17 @@
+package com.osmosoft.cryptoshield.crypto.utils
+
+import java.security.MessageDigest
+import kotlin.text.Charsets.UTF_8
+
+ class CryptoUtils {
+
+     //return a digest(checksum) byte array
+    private fun md5(str: String): ByteArray = MessageDigest.getInstance("MD5").digest(str.toByteArray(UTF_8))
+
+     //return a hex presentation of byte array
+    private fun ByteArray.toHex() = joinToString("") { "%02x".format(it) }
+
+     fun toMD5Hash(msg: String): String{
+        return md5(msg).toHex()
+    }
+}
