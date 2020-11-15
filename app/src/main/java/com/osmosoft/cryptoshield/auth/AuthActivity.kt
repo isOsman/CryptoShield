7728dad4da9e47d761b31fbdfc923eb1986cde79
Osman <niyazovo@inbox.ru>
2020-11-15 15:11:37 +0600

    added navigation from AuthActivity to UserDataActivity

--- app/src/main/java/com/osmosoft/cryptoshield/auth/AuthActivity.kt
+++ app/src/main/java/com/osmosoft/cryptoshield/auth/AuthActivity.kt
@@ -1,14 +1,19 @@
 package com.osmosoft.cryptoshield.auth
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.osmosoft.cryptoshield.R
+import com.osmosoft.cryptoshield.common.AbstractTextWatcher
 import com.osmosoft.cryptoshield.crypto.storage.KeyHelper
+import com.osmosoft.cryptoshield.crypto.utils.CryptoUtils
 import com.osmosoft.cryptoshield.data.UserAuthManager
+import com.osmosoft.cryptoshield.presentation.UserDataActivity
 
 private const val TAG = "AuthActivityTag"
 
@@ -21,29 +26,34 @@ class AuthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
 
-//        KeyHelper.init(this)
-//        KeyHelper.encrypt(this,"sdf")
+        KeyHelper.init(this)
 
+        passwordEditText = findViewById(R.id.activity_auth_password_edit_text)
+        continueButton = findViewById(R.id.activity_auth_continue_button)
 
-//        passwordEditText = findViewById(R.id.activity_auth_password_edit_text)
-//        continueButton = findViewById(R.id.activity_auth_continue_button)
-//
-//        //validate user input
-//        passwordEditText.addTextChangedListener(object: AbstractTextWatcher(){
-//            override fun afterTextChanged(s: Editable?) {
-//                    continueButton.isEnabled = s.toString().isNotEmpty() and s.toString().isNotBlank()
-//
-//            }
-//        })
+        //validate user input
+        passwordEditText.addTextChangedListener(object: AbstractTextWatcher(){
+            override fun afterTextChanged(s: Editable?) {
+                    continueButton.isEnabled = s.toString().isNotEmpty() and s.toString().isNotBlank()
+            }
+        })
 
-//        user auth
-//         val auth = UserAuthManager.getInstance(this).auth("pa ss")
-//        if(auth){
-//            Toast.makeText(this,"OK",Toast.LENGTH_SHORT).show()
-//        }else{
-//            Toast.makeText(this,"Wrong password",Toast.LENGTH_SHORT).show()
-//        }
-
+        continueButton.setOnClickListener {
+            val userInput = passwordEditText.text.toString()
+            val hashedUserInput = CryptoUtils.toMD5Hash(userInput)
+            //user auth
+            val auth = UserAuthManager.getInstance(this).auth(hashedUserInput)
+            if(!auth){
+                //if user is not authenticated - show message
+                Toast.makeText(this@AuthActivity,getString(R.string.wrong_password),Toast.LENGTH_LONG).show()
+            }else{
+                navigateToUserDataActivity()
+            }
+        }
     }
 
+    private fun navigateToUserDataActivity(){
+        val intent = Intent(this,UserDataActivity::class.java)
+        startActivity(intent)
+    }
 }
